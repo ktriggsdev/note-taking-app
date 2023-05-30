61b9540c4974b9444caee10e97d1fc5692fe40c3
Kieran Triggs <127336461+ktriggsdev@users.noreply.github.com>
2023-05-30 10:48:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,17 @@ note_text = st.text_input("Enter your note text:")
 if st.button("Save Note"):
     # Save the note to a list
     notes.append(note_text)
+    # Save notes to a file
+    with open("notes.txt", "w") as file:
+        for note in notes:
+            file.write(f"{note}\n")
     st.success("Note saved successfully!")
     st.write("Notes:")
     
     for i, note in enumerate(notes):
         st.write(f"{i + 1}. {note}")
+        
+    # Read notes from the file
+    with open("notes.txt", "r") as file:
+        for i, note in enumerate(file.readlines()):
+            st.write(f"{i + 1}. {note.strip()}")
