bb27d0a1301d3221c148b1deee7044cc21ec4bd5
Kieran Triggs <127336461+ktriggsdev@users.noreply.github.com>
2023-05-30 10:43:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ import streamlit as st
 
 st.title("Note.io")
 
+notes = ""
+
 # Input text for the note
 note_text = st.text_input("Enter your note text:")
 
 # Button to save the note
 if st.button("Save Note"):
     # Save the note to a list
-    notes = notes.append(note_text)
+    notes.append(note_text)
     st.success("Note saved successfully!")
     st.write("Notes:")
     
